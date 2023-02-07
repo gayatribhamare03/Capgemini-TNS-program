45b251078b55b3cad4c54d2f5fb67b409c512ed0
gayat <gayat@DESKTOP-VOB3RIN>
2023-02-07 13:48:27 +0530

    Hello

---
+++ tmt/src/day/r1.java
@@ -0,0 +1,21 @@
+package day;
+
+public class r1 {
+	
+	int r=45;
+	String k = "almost";
+	String h = "done";
+	
+	public void j1(String a, int b)
+	{
+		System.out.println("can you");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       r1 g= new r1();
+       g.j1("blue", 76);
+	}
+}
+
