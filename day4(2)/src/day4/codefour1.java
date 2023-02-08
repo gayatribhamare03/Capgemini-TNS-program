bd06286ce2750bc76fca92bcb00fb8f30365f77b
gayat <gayat@DESKTOP-VOB3RIN>
2023-02-08 13:52:42 +0530

    day4

---
+++ day4(2)/src/day4/codefour1.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class codefour1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=4;
+		x-=5;
+		System.out.println("value is "+x);
+		
+	}
+
+}
