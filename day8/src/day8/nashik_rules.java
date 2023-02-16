a84fca2b7ccae733648758637875cb9e1db70192
gayat <gayat@DESKTOP-VOB3RIN>
2023-02-16 14:15:54 +0530

    day8

---
+++ day8/src/day8/nashik_rules.java
@@ -0,0 +1,10 @@
+package day8;
+
+public class nashik_rules extends selto {
+	public static void main(String[]args) {
+	
+	 selto obj = new selto ();
+	 obj.disp();
+	}
+
+}
---
+++ day8/src/day8/rules.java
@@ -0,0 +1,22 @@
+package day8;
+
+public class rules {
+	
+		int maxspeed =60;
+		
+      }
+class selto extends rules
+{
+	int maxspeed=170;
+	void disp()
+	{
+		System.out.println("speed is "+super.maxspeed);
+		
+	}
+
+
+	public static void main(String[] args) {
+	 
+	}
+
+}
